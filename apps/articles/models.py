97b321c4f0b990857f8d607bc69df1fd4f185f61
create <createcheng@yeah.net>
2018-07-02 17:12:26 +0800

    创建模型

--- apps/articles/models.py
+++ apps/articles/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='分类名')
+
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=50, verbose_name='标题')
+    desc = models.CharField(max_length=100, blank=True, verbose_name='描述')
+    context = models.TextField(verbose_name='内容')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+    view = models.IntegerField(verbose_name='查看次数')
+    category = models.ForeignKey(Category, related_name='articles', on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
