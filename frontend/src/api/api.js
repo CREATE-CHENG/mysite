9f21ae75620bde4cc24e7182db7eccb1eef198e9
create <createcheng@yeah.net>
2018-07-30 16:49:52 +0800

    修改api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -7,9 +7,9 @@ export const getcategories = () => { return axios.get(`${host}/api/categories/`)
 export const getarticle = articleid => { return axios.get(`${host}/api/articles/${articleid}/`) }
 export const delarticle = articleid => { return axios.delete(`${host}/api/articles/${articleid}/`) }
 export const getarchive = () => { return axios.get(`${host}/api/archive/`) }
-export const getjwt = () => { return axios.get(`${host}/social_to_jwt/`) }
+export const getjwt = () => { return axios.get(`${host}/api/social_to_jwt/`) }
 export const logout = () => { return axios.get(`${host}/api-auth/logout/`) }
-export const checkpermission = () => { return axios.get(`${host}/check_permission/`) }
+export const checkpermission = () => { return axios.get(`${host}/api/check_permission/`) }
 export const imageupload = params => { return axios.post(`${host}/api/images/`, params, {headers: { 'Content-Type': 'multipart/form-data' }}) }
 export const imagedel = imageid => { return axios.delete(`${host}/api/images/${imageid}`) }
 export const addcomment = params => { return axios.post(`${host}/api/comments/`, params) }
